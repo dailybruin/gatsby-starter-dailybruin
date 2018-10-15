d6f6b2fd42719d989111d02ac32e410164cfdbfb
Nathan Smith <nathan@nathansmith.io>
2018-10-15 09:47:57 -0700

    Add typography.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,14 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  plugins: ['gatsby-plugin-typescript', 'gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-typescript',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-typography',
+      options: {
+        pathToConfigModule: 'src/utils/typography',
+      },
+    },
+  ],
 }
