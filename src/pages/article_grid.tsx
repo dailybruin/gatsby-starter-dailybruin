78a2427687656d5a71fad2657c5e861d58c9daea
Karl Huang <karl.huang2009@gmail.com>
2018-11-13 19:56:06 -0800

    finally got .editorconfig/prettier to work

--- src/pages/article_grid.tsx
+++ src/pages/article_grid.tsx
@@ -1,6 +1,12 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
-import { Article, CoverPhoto, Footer, Head, Headline,ArticleGrid,convertEdgesToArticles } from '@dailybruin/lux'
+import {
+  Footer,
+  Head,
+  Headline,
+  ArticleGrid,
+  convertEdgesToArticles,
+} from '@dailybruin/lux'
 import * as uniq from 'lodash.uniq'
 import capitalizeSection from '../utils/capitalizeSection'
 import { css } from 'react-emotion'
@@ -31,43 +37,43 @@ export const query = graphql`
   }
 `
 
-export default function ArticleGridPage({data}){
-  const sections=uniq(
-    data.allGoogleSheetRow.edges.map(edge=>
-    capitalizeSection(edge.node.section))
+export default function ArticleGridPage({ data }) {
+  const sections = uniq(
+    data.allGoogleSheetRow.edges.map(edge =>
+      capitalizeSection(edge.node.section)
+    )
   )
 
-  const articleGrids=sections.map((section,i) => {
+  const articleGrids = sections.map((section, i) => {
     const articles = convertEdgesToArticles(
       data.allGoogleSheetRow.edges
     ).filter(article => article.section === section.toLowerCase())
     return <ArticleGrid key={i} heading={section} articles={articles} />
   })
 
-  return(
-  <>
-  <div
-    className={css`
-      margin: 0 auto;
-      max-width: 1080px;
-      padding: 0px 1.0875rem 1.45 rem;
-      padding-top: 0px;
-    `}
-    >
-  <Head {...data.site.siteMetadata} />
-  <div
-    className={css`
-      padding-top:45px;
-      width: 100%;
-      text-align: center;
-      `
-    }
-    >
-  <Headline text="Article Grid Gatsby Starter Page"/>
-  </div>
-    {articleGrids}
-  </div>
-    <Footer developers="Karl Huang" copyrightYear={2018} />
-  </>
-)
+  return (
+    <>
+      <div
+        className={css`
+          margin: 0 auto;
+          max-width: 1080px;
+          padding: 0px 1.0875rem 1.45 rem;
+          padding-top: 0px;
+        `}
+      >
+        <Head {...data.site.siteMetadata} />
+        <div
+          className={css`
+            padding-top: 45px;
+            width: 100%;
+            text-align: center;
+          `}
+        >
+          <Headline text="Article Grid Gatsby Starter Page" />
+        </div>
+        {articleGrids}
+      </div>
+      <Footer developers="Karl Huang" copyrightYear={2018} />
+    </>
+  )
 }
